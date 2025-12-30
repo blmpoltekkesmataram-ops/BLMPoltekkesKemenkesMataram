054d569c55a6d4adf3f1a1d32d3bfd13be4665fe
blmpoltekkesmataram-ops <blmpoltekkesmataram@gmail.com>
2025-12-30 19:29:59 +0800

    time 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BLMPoltekkesKemenkesMataram/',
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
