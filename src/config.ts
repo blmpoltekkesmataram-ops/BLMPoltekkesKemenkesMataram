e15c3e6bc103a554465aff7ad5edb99309e727f6
blmpoltekkesmataram-ops <blmpoltekkesmataram@gmail.com>
2025-12-30 19:09:03 +0800

    null

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * KONFIGURASI API CLOUDFLARE
- * Ganti URL di bawah ini dengan URL .workers.dev yang Anda dapatkan 
- * setelah men-deploy Worker Anda di Cloudflare.
- */
-
-export const API_URL = 'https://blm-api.SILAKAN-GANTI-INI.workers.dev';
